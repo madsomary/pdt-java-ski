4886ae4db8d6287fccee4089aba252d518aa7382
madsomary <marymadso88@gmail.com>
2015-05-05 11:15:00 +0500

    add local variable in 'type' method

--- addressbook-selenium-tests/src/com/example/fw/HelperBase.java
+++ addressbook-selenium-tests/src/com/example/fw/HelperBase.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public abstract class HelperBase {
@@ -41,8 +42,9 @@ public abstract class HelperBase {
 		  }
 		protected void type(By locator, String text) {
 			if (text != null) {
-			driver.findElement(locator).clear();
-			driver.findElement(locator).sendKeys(text);
+			WebElement elementLocator = driver.findElement(locator);
+			elementLocator.clear();
+			elementLocator.sendKeys(text);
 		  }
 		}
 		protected void click(By locator) {
