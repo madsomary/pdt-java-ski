10fe98a09782038f84ef4b0caceb911b054ba30e
madsomary <marymadso88@gmail.com>
2015-05-06 15:54:44 +0500

    HW 5 + HW 7 for groups

--- addressbook-selenium-tests/src/com/example/fw/GroupHelper.java
+++ addressbook-selenium-tests/src/com/example/fw/GroupHelper.java
@@ -4,46 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.By.ByName;
 import org.openqa.selenium.WebElement;
 
 import com.example.tests.GroupData;
 
-public class GroupHelper extends HelperBase{
+public class GroupHelper extends HelperBase {
 
 	public GroupHelper(ApplicationManager manager) {
 		super(manager);
-      }
+	}
 
 	public void initGroupCreation() {
 		click(By.name("new"));
-	  }
+	}
 
 	public void fillGroupForm(GroupData group) {
 		type(By.name("group_name"), group.name);
 		type(By.name("group_header"), group.header);
 		type(By.name("group_footer"), group.footer);
-	  }
+	}
 
 	public void submitGroupCreation() {
-	    click(By.name("submit"));
-	  }
+		click(By.name("submit"));
+	}
 
 	public void returnToGroupsPage() {
 		click(By.linkText("group page"));
-	  }
-	
-	private void selectGroupByIndex(int index) {
-		click(By.xpath("//input[@name='selected[]'][" + index + "]"));
 	}
-	
+
+	private void selectGroupByIndex(int index) {
+		click(By.xpath("//input[@name='selected[]'][" + (index + 1) + "]"));
+	}
+
 	public void deleteGroup(int index) {
-		selectGroupByIndex(index);	
-		click(By.name("delete"));			
-		}
+		selectGroupByIndex(index);
+		click(By.name("delete"));
+	}
 
 	public void initGroupModification(int index) {
-		selectGroupByIndex(index);		
+		selectGroupByIndex(index);
 		click(By.name("edit"));
 	}
 
@@ -53,11 +52,13 @@ public class GroupHelper extends HelperBase{
 
 	public List<GroupData> getGroups() {
 		List<GroupData> groups = new ArrayList<GroupData>();
-		List<WebElement> checkboxes = driver.findElements(By.name("selected[]"));
+		List<WebElement> checkboxes = driver
+				.findElements(By.name("selected[]"));
 		for (WebElement checkbox : checkboxes) {
 			GroupData group = new GroupData();
-			String title= checkbox.getAttribute("title");
-			group.name = title.substring("Select(".length(), title.length() - ")".length());
+			String title = checkbox.getAttribute("title");
+			group.name = title.substring("Select (".length(), title.length()
+					- ")".length());
 			groups.add(group);
 		}
 		return groups;
--- addressbook-selenium-tests/src/com/example/tests/GroupData.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupData.java
@@ -1,31 +1,35 @@
 package com.example.tests;
 
-public class GroupData implements Comparable<GroupData>{
+public class GroupData implements Comparable<GroupData> {
 	public String name;
 	public String header;
 	public String footer;
 
 	public GroupData() {
-    }
+	}
+
 	public GroupData(String groupname, String header, String footer) {
 		this.name = groupname;
 		this.header = header;
 		this.footer = footer;
 	}
-	
+
+
+
 	@Override
 	public String toString() {
-		return "GroupData [name=" + name + "]";
+		return "GroupData [name=" + name + ", header=" + header + ", footer="
+				+ footer + "]";
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-	//	result = prime * result + ((name == null) ? 0 : name.hashCode());
+		// result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -47,7 +51,5 @@ public class GroupData implements Comparable<GroupData>{
 	public int compareTo(GroupData other) {
 		return this.name.toLowerCase().compareTo(other.name.toLowerCase());
 	}
-	
-	
-	
+
 }
